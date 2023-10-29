804a2249d98766358303c6685c90a06366046e7f
Asamiyaa <736996211@qq.com>
2023-10-29 22:51:56 +0800

    Update Qiyewx.py

--- Quantification/send/Qiyewx.py
+++ Quantification/send/Qiyewx.py
@@ -24,6 +24,29 @@ def send_text(content, mentioned_list=None, mentioned_mobile_list=None):
     info = requests.post(url=webhook, data=data, headers=header)
 
 
+def send_text_B(content, mentioned_list=None, mentioned_mobile_list=None):
+
+    webhook = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0c662554-5b02-48e6-9b24-3c70c0428a92"
+
+    header = {
+        "Content-Type": "application/json",
+        "Charset": "UTF-8"
+    }
+    data = {
+
+        "msgtype": "text",
+        "text": {
+            "content": content
+            , "mentioned_list": mentioned_list
+            , "mentioned_mobile_list": mentioned_mobile_list
+        }
+    }
+    data = json.dumps(data)
+    info = requests.post(url=webhook, data=data, headers=header)
+
+
+
+
 # 发送markdown消息
 def send_md(webhook, content):
     header = {
